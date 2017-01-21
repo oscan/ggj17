239fa0ce14f61588bb90e94eb66fd874da5da2d9
Owen Canavan <ocanavan@gmail.com>
2017-01-21 11:39:12 +0000

    working swiping

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData : MonoBehaviour {
+
+	public bool tweening = false;
+	protected Transform tweening_end_transform;
+	protected Vector3 tweening_start_transform_pos;
+	protected Vector3 tweening_start_transform_ang;
+	protected Vector3 tweening_start_transform_scale;
+	protected float tween_start = 0;
+	protected float tween_dur;
+	protected Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(tweening_end_transform != null) {
+			float p = (Time.time - tween_start)/tween_dur;
+			if(p > 1) {
+				p = 1;
+			}
+
+			Vector3 dp = tweening_end_transform.position - tweening_start_transform_pos;
+
+			Vector3 pos = transform.position;
+			pos.x = tweening_start_transform_pos.x + (dp.x * p);
+			pos.y = tweening_start_transform_pos.y + (dp.y * p);
+			pos.z = tweening_start_transform_pos.z + (dp.z * p);
+			tf.position = pos;
+
+			Vector3 dAng = tweening_end_transform.eulerAngles - tweening_start_transform_ang;
+			Vector3 ang = tweening_start_transform_ang + dAng*p;
+			tf.eulerAngles = ang;
+
+			Vector3 dScale = tweening_end_transform.localScale - tweening_start_transform_scale;
+			Vector3 scale = tweening_start_transform_scale + dScale*p;
+			tf.localScale = scale;
+
+			if(p == 1) {
+				tweening_end_transform = null;
+			}
+		}
+	}
+
+	public void tweenTo(Transform t, float dur) {
+		tweening_start_transform_pos.x = tf.position.x;
+		tweening_start_transform_pos.y = tf.position.y;
+		tweening_start_transform_pos.z = tf.position.z;
+
+		tweening_start_transform_ang = tf.eulerAngles;
+
+		tweening_start_transform_scale.x = tf.localScale.x;
+		tweening_start_transform_scale.y = tf.localScale.y;
+		tweening_start_transform_scale.z = tf.localScale.z;
+
+		tweening_end_transform = t;
+		tween_start = Time.time;
+		tween_dur = dur;
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject cur_card;
+	public GameObject slot1;
+	public GameObject slot2;
+	public GameObject slot3;
+
+	public Transform present;
+	public Transform discard;
+	public Transform keep;
+
+	protected bool swiping = false;
+	protected Vector2 swipeStart = Vector2.zero;
+	protected Vector2 delta = Vector2.zero;
+	protected bool waitingForSwipe = false;
+
+	// Use this for initialization
+	void Start () {
+		CardData card = cur_card.GetComponent<CardData>();
+		card.tweenTo(present, 1f);
+		waitingForSwipe = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(waitingForSwipe) {
+			if(!swiping && Input.GetMouseButtonDown(0)) {
+				swiping = true;
+				swipeStart.x = Input.mousePosition.x;
+				swipeStart.y = Input.mousePosition.y;
+			} else if(swiping) {
+				delta.x = Input.mousePosition.x - swipeStart.x;
+				delta.y = Input.mousePosition.y - swipeStart.y;
+				swiping = false;
+				int dx = 0;
+				if(delta.x > 0) { 
+					dx = 1;
+				} else if (delta.x < 0) {
+					dx = -1;
+				}
+				onSwipe(dx);
+			}
+		}
+
+		if(Input.GetKeyUp(KeyCode.D)) {
+			CardData card = cur_card.GetComponent<CardData>();
+			card.tweenTo(present, 1f);
+		}
+	}
+
+	void onSwipe(int dx) {
+		CardData card = cur_card.GetComponent<CardData>();
+		if(waitingForSwipe) {
+			if(dx < 0) {
+				//reject card
+				card.tweenTo(discard, 0.5f);
+			} else if(dx > 0) {
+				//select card
+				card.tweenTo(keep,0.5f);
+			}
+		}
+	}
+
+	void draft() {
+		
+	}
+}
