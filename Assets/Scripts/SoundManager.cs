b33b4ca2e41b63a31ec15b4c39672f6fb311a569
Colm Larkin <clarkin@gmail.com>
2017-01-21 17:16:40 +0000

    more music

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,6 +5,7 @@ public class SoundManager : MonoBehaviour
 {
 	public AudioSource efxSource;              
 	public AudioSource musicSource;   
+	public AudioSource underwaterMusicSource;   
 	public AudioSource underwaterAmbientSource;   
 	public static SoundManager instance = null;  
 
